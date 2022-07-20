e42fffd8a8bd2f54136f6db89062b907097b472b
TKMA5FE <scott.stella@kohls.com>
2022-07-20 09:44:57 -0400

    Adding header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Nav from "./components/Nav/Nav";
 import Article from "./components/Article/Article";
 import Aside from "./components/Aside/Aside";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Nav />
       <Article />
       <Aside />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div class=".header">
+      <h1>Showcase React</h1>
+    </div>
+  );
+}
