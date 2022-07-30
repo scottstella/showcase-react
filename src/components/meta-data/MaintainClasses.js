b6be02b5b7f8d06674bce6aeea5c72582a89ce45
TKMA5FE <scott.stella@kohls.com>
2022-07-30 18:42:35 -0400

    adding supabase for hero class

--- src/components/meta-data/MaintainClasses.js
+++ src/components/meta-data/MaintainClasses.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { supabase } from "../../supabase/Client";
 
 export default function MaintainClasses() {
-  return <div>MaintainClasses</div>;
+  const [heroClasses, setHeroClasses] = useState([]);
+  const [heroClass, setHeroClass] = useState({ name: "" });
+  const { name } = heroClass;
+
+  useEffect(() => {
+    fetchHeroClasses();
+  }, []);
+
+  async function fetchHeroClasses() {
+    const { data } = await supabase.from("hero_class").select();
+    setHeroClasses(data);
+    console.log("data: ", data);
+  }
+
+  async function addHeroClass() {
+    await supabase.from("hero_class").insert([{ name }]).single();
+    setHeroClass({ name: "" });
+    fetchHeroClasses();
+  }
+  return (
+    <div>
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setHeroClass({ ...heroClass, name: e.target.value })}
+      />
+      <button onClick={addHeroClass}>Add Hero Class</button>
+      {heroClasses.map((heroClass) => (
+        <div key={heroClass.id}>
+          <p>{heroClass.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
