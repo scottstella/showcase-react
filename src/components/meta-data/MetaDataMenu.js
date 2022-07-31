3b9457d201a6d65e7b5868cf010fb0fac86ac9c1
Scott Stella <scott@stella.ws>
2022-07-31 00:49:04 -0400

    hilighting selected tab

--- src/components/meta-data/MetaDataMenu.js
+++ src/components/meta-data/MetaDataMenu.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MetaDataMenu.css";
 
 export default function MetaDataMenu(props) {
+  const [menuSelection, setMenuSelection] = useState("");
+
   const makeMenuSelection = (e) => {
+    setMenuSelection(e.target.id);
     props.onSelectMetaData(e.target.id);
   };
 
   return (
     <div>
       <div className="meta-data-menu">
-        <div id="sets" onClick={makeMenuSelection}>
+        <div
+          id="sets"
+          onClick={makeMenuSelection}
+          class={`${
+            menuSelection === "sets" ? "meta-data-menu__selected" : ""
+          }`}
+        >
           Sets
         </div>
-        <div id="classes" onClick={makeMenuSelection}>
+        <div
+          id="classes"
+          onClick={makeMenuSelection}
+          class={`${
+            menuSelection === "classes" ? "meta-data-menu__selected" : ""
+          }`}
+        >
           Classes
         </div>
-        <div id="tribes" onClick={makeMenuSelection}>
+        <div
+          id="tribes"
+          onClick={makeMenuSelection}
+          class={`${
+            menuSelection === "tribes" ? "meta-data-menu__selected" : ""
+          }`}
+        >
           Tribes
         </div>
       </div>
