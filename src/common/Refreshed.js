9ed3ea286e43cd9ae393651be41e31c0286bf9e1
scott.stella <scott@stella.ws>
2022-08-03 22:57:09 -0400

    Padding timestamp values with leading 0

--- src/common/Refreshed.js
+++ src/common/Refreshed.js
@@ -6,15 +6,15 @@ export default function Refreshed(props) {
     return (
       date.getFullYear() +
       "-" +
-      (date.getMonth() + 1) +
+      ((date.getMonth() + 1) < 10? "0" + (date.getMonth()+1) : (date.getMonth()+1)) +
       "-" +
-      date.getDate() +
+      (date.getDate() <10? "0" + date.getDate() : date.getDate()) +
       " " +
-      date.getHours() +
+      (date.getHours() <10? "0" + date.getHours() : date.getHours()) +
       ":" +
-      date.getMinutes() +
+      (date.getMinutes() <10? "0" + date.getMinutes() : date.getMinutes()) +
       ":" +
-      date.getSeconds()
+      (date.getSeconds() <10? "0" + date.getSeconds() : date.getSeconds())
     );
   };
   return (
