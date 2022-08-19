ab9d1a3fc5d3784858d8457db29b08107f20c650
TKMA5FE <scott.stella@kohls.com>
2022-08-19 17:36:15 -0400

    injecting supabase client into service

--- src/services/CardService.js
+++ src/services/CardService.js
@@ -1,18 +1,22 @@
-import { supabase } from "../supabase/Client";
+import { supabase as supabaseImpl } from "../supabase/Client";
 
 export class CardService {
+
+  constructor(supabase) {
+    this.supabase = supabase;
+  }
   async fetchHeroClasses() {
-    return await supabase.from("hero_class").select();
+    return await this.supabase.from("hero_class").select();
   }
 
   async deleteHeroClass(id) {
-    return await supabase.from("hero_class").delete().eq("id", id);
+    return await this.supabase.from("hero_class").delete().eq("id", id);
   }
 
   async addHeroClass(heroClass) {
     const name = heroClass.name;
-    return await supabase.from("hero_class").insert([{ name }]).single();
+    return await this.supabase.from("hero_class").insert([{ name }]).single();
   }
 }
 
-export default new CardService();
+export default new CardService(supabaseImpl);
